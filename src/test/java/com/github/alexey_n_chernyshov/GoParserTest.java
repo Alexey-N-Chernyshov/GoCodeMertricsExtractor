d48b1e02b50dd5323824048b5a2407d46619b34f
Alexey Chernyshov <Alexey.N.Chernyshov@gmail.com>
2017-02-12 10:29:43 +0300

    add literals

--- src/test/java/com/github/alexey_n_chernyshov/GoParserTest.java
+++ src/test/java/com/github/alexey_n_chernyshov/GoParserTest.java
@@ -147,4 +147,298 @@ public class GoParserTest {
         }
     }
 
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals1() {
+        String src = "0i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals2() {
+        String src = "0.i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals3() {
+        String src = "1.e+0i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals4() {
+        String src = "6.67428e-11i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals5() {
+        String src = "1E6i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests imaginary literals for GoParser.
+     */
+    @Test
+    public void testImaginaryLiterals6() {
+        String src = ".25i";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals1() {
+        String src = "'a'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals2() {
+        String src = "'ä'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals3() {
+        String src = "'\\t'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals4() {
+        String src = "'\\007'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals5() {
+        String src = "'\\xff'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals6() {
+        String src = "'\\u12e4'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     */
+    @Test
+    public void testRuneLiterals7() {
+        String src = "'\\U00101234'";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     * rune literal containing single quote character.
+     */
+    @Test
+    public void testRuneLiterals8() {
+        String src = "'\\''";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     * illegal: too many characters
+     */
+    @Test(expected = TokenMgrError.class)
+    public void testRuneLiterals9() throws ParseException {
+        String src = "'aa'";
+        GoParser parser = createParser(src);
+        parser.Program();
+    }
+
+    /**
+     * Tests rune literals for GoParser.
+     * illegal: too few hexadecimal digits
+     */
+    @Test(expected = TokenMgrError.class)
+    public void testRuneLiterals10() throws ParseException {
+        String src = "'\\xa'";
+        GoParser parser = createParser(src);
+        parser.Program();
+    }
+
+    /**
+     * Tests string literals for GoParser.
+     */
+    @Test
+    public void testStringLiterals1() {
+        String src = "`abc`";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests string literals for GoParser.
+     */
+    @Test
+    public void testStringLiterals2() {
+        String src =
+                "`\\n\n" +
+                        "\\n`";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests string literals for GoParser.
+     */
+    @Test
+    public void testStringLiterals3() {
+        String src = "\"Hello, world!\\n\"";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
+
+    /**
+     * Tests string literals for GoParser.
+     */
+    @Test
+    public void testStringLiterals4() {
+        String src = "\"\\u65e5\\U00008a9e\"";
+        GoParser parser = createParser(src);
+        try {
+            SimpleNode root = parser.Program();
+            assertEquals(getParseResult(root), src);
+        } catch (ParseException e) {
+            fail("Caught an exception " + e.toString());
+        }
+    }
 }
